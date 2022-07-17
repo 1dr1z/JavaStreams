49cb5485fb8bf41fd464400650c70f9977b6697f
Idriz Mehmedovic <idriz.96@hotmail.com>
2022-07-17 16:21:32 +0200

    init commit

--- src/test/java/lectures/Lecture1.java
+++ src/test/java/lectures/Lecture1.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import beans.Person;
 
 import com.google.common.collect.ImmutableList;
+
 import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -16,17 +17,34 @@ import org.junit.Test;
 
 public class Lecture1 {
 
-  @Test
-  public void imperativeApproach() throws IOException {
-    List<Person> people = MockData.getPeople();
-    // 1. Find people aged less or equal 18
-    // 2. Then change implementation to find first 10 people
+    @Test
+    public void imperativeApproach() throws IOException {
+        List<Person> people = MockData.getPeople();
+        // 1. Find people aged less or equal 18
+        // 2. Then change implementation to find first 10 people
+        List<Person> youngPeople = Lists.newArrayList();
+        final int limit = 10;
+        int counter = 0;
 
-  }
+        for(Person person:people){
+            if(person.getAge()<=18){
+                youngPeople.add(person);
+                counter++;
+                if(counter==limit){
+                    break;
+                }
+            }
+        }
 
-  @Test
-  public void declarativeApproachUsingStreams() throws Exception {
-    ImmutableList<Person> people = MockData.getPeople();
+        for(Person young:youngPeople){
+            System.out.println(young);
+        }
 
-  }
+    }
+
+    @Test
+    public void declarativeApproachUsingStreams() throws Exception {
+        ImmutableList<Person> people = MockData.getPeople();
+
+    }
 }
